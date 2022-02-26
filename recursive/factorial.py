f351d594cd59e150797b84e0c362029a7ef552e3
kalinga <skalinga29@gmail.com>
2022-02-26 16:10:50 +0530

    error handling

--- recursive/factorial.py
+++ recursive/factorial.py
@@ -1,5 +1,5 @@
 def factorial(n):
-
+    assert n >= 0 and int(n) == n, 'Number must be positive integer'
     if n == 1:
         return 1
     return n * factorial(n-1)
--- recursive/fibonacci.py
+++ recursive/fibonacci.py
@@ -1,5 +1,5 @@
 def fibonacci(n):
-
+    assert n >= 0 and int(n) == n, 'Number must be positive integer'
     if n == 1 or n == 2:
         return 1
 
